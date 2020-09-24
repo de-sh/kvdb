5d4c02d5cbd5b354924fde3fac7c6eee10aa74cf
Devdutt Shenoi <devdutt@outlook.in>
2020-09-24 22:15:38 +0530

    Move executive code from store into repl

--- src/repl.rs
+++ src/repl.rs
@@ -1,16 +1,22 @@
-use std::fmt;
+/// Code pertaining to environment management of the REPL. 
+/// Provides an interface to operate on the Storage Engine.
+
 use std::io;
 use std::io::Write;
 
 use crate::parser::{MetaCmdResult, Statement, StatementType};
-use crate::store::Store;
+use crate::store::{ExecResult, Store};
 
+/// The REPL struct is used to hold environment variables relating to the REPL.
 pub struct REPL {
+    /// User input read from the CLI, in string form.
     cmd: String,
+    /// Storage Engine currently in use.
     store: Store,
 }
 
 impl REPL {
+    /// Create a new instance of the REPL.
     pub fn new() -> Self {
         REPL {
             cmd: "".to_owned(),
@@ -18,17 +24,19 @@ impl REPL {
         }
     }
 
+    /// Starts REPL execution in earnest.
     pub fn repl(&mut self) {
         loop {
             // Prompt
             print!("db > ");
             // Read
             self.read_input();
-            // Evaluate
+            // Evaluate and Print/Execute
             self.parse_input();
         }
     }
 
+    /// Takes input from the REPL user.
     fn read_input(&mut self) {
         let mut cmd = "".to_owned();
         io::stdout().flush().expect("Error");
@@ -38,25 +46,45 @@ impl REPL {
         self.cmd = cmd.trim().to_string();
     }
 
+    /// Parses Commands from the REPL. If Meta, executes on REPL environment,
+    /// otherwise executes them on the Storage Engine.
     fn parse_input(&mut self) {
-        // Parse Meta Commands or Execute them on storage
+        // Meta commands start with `.`.
         if self.cmd.starts_with(".") {
             match MetaCmdResult::run(&self.cmd) {
-                MetaCmdResult::Unrecognized => println!("db: command not found: {}", self.cmd),
+                MetaCmdResult::Unrecognized => println!("db: meta command not found: {}", self.cmd),
                 MetaCmdResult::Success => {}
             }
         } else {
             let st = Statement::prep(&self.cmd);
-            match st.stype {
-                StatementType::Unrecognized => println!("db: command not found: {}", self.cmd),
-                _ => self.store.execute(st),
+            let key = st.key.unwrap();
+            // If type of statement is legit, execute, else fail.
+            match match st.stype {
+                StatementType::Set => self.store.set(key, st.value.unwrap()),
+                StatementType::Get => {
+                    match self.store.get(key) {
+                        Ok(res) => {
+                            println!("{}", res);
+                            ExecResult::Success
+                        },
+                        Err(ExecResult::Failed) => {
+                            // If the key doesn't exist, get() explicitly returns this,
+                            // so print the desired Error message.
+                            eprintln!("Error: No value associated with key `{}`.", *key);
+                            ExecResult::Failed
+                        },
+                        _ => ExecResult::Failed,
+                    }
+                },
+                StatementType::Rem => self.store.rem(key),
+                StatementType::Unrecognized => {
+                    println!("db: command not found: {}", self.cmd);
+                    ExecResult::Failed
+                }
+            } {
+                ExecResult::Failed => eprintln!("Command Execution Failed."),
+                ExecResult::Success => println!("Success: OK"),
             }
         }
-    }
-}
-
-impl fmt::Display for REPL {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "({})", self.cmd)
     }
 }
